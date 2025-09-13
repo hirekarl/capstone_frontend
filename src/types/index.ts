dfb6bd8ff31ab22a67d52e3e0b4d9277a17862e6
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-13 13:52:34 -0400

    Added interfaces.

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface UserType {
+  _id: string
+  username: string
+  email: string
+}
+
+export interface ProjectType {
+  _id: string
+  name: string
+  description: string
+  owner: string
+}
+
+export type TaskStatusType = "To Do" | "In Progress" | "Done"
+
+export interface TaskType {
+  _id: string
+  title: string
+  description: string
+  status: TaskStatusType
+  project: string
+}
