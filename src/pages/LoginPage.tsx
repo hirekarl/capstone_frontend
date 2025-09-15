f3761e6d6831e12ed4574e318f0b223b27642d7a
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-15 11:51:07 -0400

    Began work on LoginPage.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,27 @@
+import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import type { UserLoginFormDataType } from "../types"
+
+import { AuthContext } from "../contexts/AuthContext"
+import type { AuthContextType } from "../contexts/AuthContext"
+
 export default function LoginPage() {
+  const { isAuthenticated, login } = useContext<AuthContextType>(AuthContext)
+  const [userLoginFormData, setUserLoginFormData] =
+    useState<UserLoginFormDataType | null>(null)
+
+  const navigate = useNavigate()
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
+  if (isAuthenticated) navigate("/projects")
+
   return <div>LoginPage</div>
 }
