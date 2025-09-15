d8415add40b1d0e17b2c266a3afa63018be52eb3
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-15 11:37:09 -0400

    Added Register button to NavBar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,86 @@
-export default function NavBar() {
-  return <div>NavBar</div>
+import { Link, NavLink } from "react-router-dom"
+import { useContext } from "react"
+import { AuthContext, type AuthContextType } from "../contexts/AuthContext"
+
+const NavBar = () => {
+  const { isAuthenticated, logout } = useContext<AuthContextType>(AuthContext)
+
+  const handleLogout = () => {
+    if (logout) {
+      logout()
+    }
+  }
+
+  const authContent = !isAuthenticated ? (
+    <>
+      <li className="nav-item">
+        <NavLink
+          to="/login"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Login
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/register"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Register
+        </NavLink>
+      </li>
+    </>
+  ) : (
+    <li className="nav-item d-flex align-items-center">
+      <button
+        type="button"
+        className="btn btn-sm btn-primary"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+    </li>
+  )
+
+  return (
+    <nav className="sticky-top navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link to={"/"} className="navbar-brand">
+          Pro-Tasker
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="#navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                to="/projects"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Projects
+              </NavLink>
+            </li>
+            {authContent}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
+
+export default NavBar
