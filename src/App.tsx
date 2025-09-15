4b5f1b7c32b92f346e2c908495e18a2e511f92d9
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-15 11:18:50 -0400

    Added routing and components to App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 
+import { Navigate, Routes, Route } from "react-router-dom"
+
+import AuthContextProvider from "./contexts/AuthContextProvider"
+
+import NavBar from "./components/NavBar"
+import ProjectsPage from "./pages/ProjectsPage"
+import ProjectDetailPage from "./pages/ProjectDetailPage"
+import LoginPage from "./pages/LoginPage"
+import Footer from "./components/Footer"
+
 function App() {
-  return <h1>Pro-Tasker</h1>
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <div className="flex-grow-1">
+        <AuthContextProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/projects" replace />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route
+              path="/projects/:projectId"
+              element={<ProjectDetailPage />}
+            />
+          </Routes>
+          <Footer />
+        </AuthContextProvider>
+      </div>
+    </div>
+  )
 }
 
 export default App
