c27a67d7b877c66edfbe906f79abe48317b2a158
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-18 11:25:17 -0400

    Added close button to AlertMessage.

--- src/components/AlertMessage.tsx
+++ src/components/AlertMessage.tsx
@@ -4,6 +4,7 @@ export default function AlertMessage({ type, message }: AlertMessageType) {
   return (
     <div className={`alert alert-${type} alert-dismissible fade show`}>
       {message}
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   )
 }
